57c309739d85b940197e13685e3d2859d3a25eac
TA1069 <maylafaat@gmail.com>
2022-11-23 15:46:47 +0700

    add public bool

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Exercise3
 {
@@ -17,5 +21,18 @@ namespace Exercise3
         {
             LAST = null;
         }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == LAST.rollNumber)/*if the node is present at the end*/
+                    return true;
+            }
+            if (rollNo == LAST.rollNumber)
+                return true;
+            else
+                return false;
+
+       }
     }
 }
