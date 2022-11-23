1e77d94441fd6803bd52bb1683aa5eab769205e2
TA1069 <maylafaat@gmail.com>
2022-11-23 16:02:59 +0700

    add case 1

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -60,6 +60,47 @@ namespace Exercise3
                 Console.Write(LAST.rollNumber + " " + LAST.name + "\n");
             }
         }
-
+        public void firstNode()
+        {
+            if (ListEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+                Console.WriteLine("\nThe first record in the list is : \n\n" + LAST.next.rollNumber + "   " + LAST.next.name);
+        }
+        static void Main(string[] args)
+        {
+            CircularList obj = new CircularList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\n Menu");
+                    Console.WriteLine("\n 1. View all the records in the list");
+                    Console.WriteLine("\n 2. Search for a record in the list");
+                    Console.WriteLine("\n 3. Display the first record in the list");
+                    Console.WriteLine("\n 4. Exit\n");
+                    Console.WriteLine("\n Enter your choice (1-4): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is Empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.WriteLine
+                            }
+                }
+            }
+        }
     }
 }
